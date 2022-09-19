36d77fad6ebc7df4229a99a367c706d5873bd7f7
vedmond <vyedkh@gmail.com>
2022-09-19 13:55:12 +0300

    feat: add module scss for ChoseTask page

--- src/pages/ChoseTask/ChoseTask.tsx
+++ src/pages/ChoseTask/ChoseTask.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import '../../scss/ChoseTask.scss'
+import style from '../../scss/ChoseTask.module.scss'
 
 export const ChoseTask = () => {
     const containerStyle = {
@@ -11,136 +11,111 @@ export const ChoseTask = () => {
   }
   return (
     <div>
-      <header className="header">
-      <div className="logo__box">
+      <header className={style.header}>
+      <div className={style.logo__box}>
         <h1><span>Art</span> Quiz</h1>
-        <div className="ellipse ellipse__right"></div>
-        <div className="ellipse ellipse__left"></div>
+        <div className={`${style.ellipse} ${style.ellipse__right}`}></div>
+        <div className={`${style.ellipse} ${style.ellipse__left}`}></div>
       </div>
-      <ul className="menu">
-        <li className="menu-items"><a href=".">Home </a></li>
-        <li className="menu-items"><a href="." className="active">Categories</a></li>
+      <ul className={style.menu}>
+        <li className={style.menu_items}><a href=".">Home </a></li>
+        <li className={style.menu_items}><a href="." className={style.active}>Categories</a></li>
       </ul>
-      <a href="/"  className="icon__cross"> </a>
+      <a href="."  className={style.icon__cross}> </a>
     </header>
-    <main className="main">
-
-     <div className="box">
-      <span className="name-category">Portrait</span>
-      <div className="counter none">7/10</div>
-      
-        <div className="pic-category dark" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
+    <main className={style.main}>
+     <div className={style.box}>
+      <span className={style.name_category}>Portrait</span>
+      <div className={`${style.counter} ${style.none}`}>7/10</div>
+        <div className={`${style.pic_category} ${style.dark}`} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
      </div>
-     <div className="box">
-      <span className="name-category">Still life</span>
-      <div className="counter ">3/10</div>
-      
-        <div className="pic-category " style={containerStyle}>
-          <div className="again "><span className="icon-again"></span><span>Play again</span></div>
+     <div className={style.box}>
+      <span className={style.name_category}>Still life</span>
+      <div className={style.counter }>3/10</div>
+        <div className={style.pic_category } style={containerStyle}>
+          <div className={style.again }><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
      </div>
-     <div className="box">
-      <span className="name-category">Kitsh</span>
-      <div className="counter none">5/10</div>
-      
-        <div className="pic-category dark" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
+     <div className={style.box}>
+      <span className={style.name_category}>Kitsh</span>
+      <div className={`${style.counter} ${style.none}`}>5/10</div>
+        <div className={`${style.pic_category} ${style.dark}`} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
      </div>
-     <div className="box">
-      <span className="name-category">Nude</span>
-      <div className="counter">8/10</div>
-      
-        <div className="pic-category" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
+     <div className={style.box}>
+      <span className={style.name_category}>Nude</span>
+      <div className={style.counter}>8/10</div>
+        <div className={style.pic_category} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
      </div>
-     <div className="box">
-      <span className="name-category">Portrait</span>
-      <div className="counter none">7/10</div>
-      
-        <div className="pic-category dark" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
+     <div className={style.box}>
+      <span className={style.name_category}>Portrait</span>
+      <div className={`${style.counter} ${style.none}`}>7/10</div>
+        <div className={`${style.pic_category} ${style.dark}`} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
      </div>
-     <div className="box">
-      <span className="name-category">Still life</span>
-      <div className="counter ">3/10</div>
-      
-        <div className="pic-category " style={containerStyle}>
-          <div className="again "><span className="icon-again"></span><span>Play again</span></div>
+     <div className={style.box}>
+      <span className={style.name_category}>Still life</span>
+      <div className={style.counter }>3/10</div>
+        <div className={style.pic_category } style={containerStyle}>
+          <div className={style.again }><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
+     </div> 
+     <div className={style.box}>
+      <span className={style.name_category}>Kitsh</span>
+      <div className={`${style.counter} ${style.none}`}>5/10</div>
+        <div className={`${style.pic_category} ${style.dark}`} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
+        </div>
      </div>
-     <div className="box">
-      <span className="name-category">Kitsh</span>
-      <div className="counter none">5/10</div>
-      
-        <div className="pic-category dark" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
+     <div className={style.box}>
+      <span className={style.name_category}>Nude</span>
+      <div className={style.counter}>8/10</div>
+        <div className={style.pic_category} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
      </div>
-     <div className="box">
-      <span className="name-category">Nude</span>
-      <div className="counter">8/10</div>
-      
-        <div className="pic-category" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
+     <div className={style.box}>
+      <span className={style.name_category}>Nude</span>
+      <div className={style.counter}>8/10</div>
+        <div className={style.pic_category} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
      </div>
-     <div className="box">
-      <span className="name-category">Nude</span>
-      <div className="counter">8/10</div>
-      
-        <div className="pic-category" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
+     <div className={style.box}>
+      <span className={style.name_category}>Nude</span>
+      <div className={style.counter}>8/10</div>
+        <div className={style.pic_category} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
      </div>
-     <div className="box">
-      <span className="name-category">Nude</span>
-      <div className="counter">8/10</div>
-      
-        <div className="pic-category" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
+      <div className={style.box}>
+        <span className={style.name_category}>Kitsh</span>
+        <div className={`${style.again} ${style.none}`}>5/10</div>
+        <div className={`${style.pic_category} ${style.dark}`} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-  
-     </div>
-      <div className="box">
-        <span className="name-category">Kitsh</span>
-        <div className="counter none">5/10</div>
-      
-        <div className="pic-category dark" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
-        </div>
-      
       </div>
-      <div className="box">
-        <span className="name-category">Nude</span>
-        <div className="counter">8/10</div>
-      
-        <div className="pic-category" style={containerStyle}>
-          <div className="again none"><span className="icon-again"></span><span>Play again</span></div>
+      <div className={style.box}>
+        <span className={style.name_category}>Nude</span>
+        <div className={style.counter}>8/10</div>
+        <div className={style.pic_category} style={containerStyle}>
+          <div className={`${style.again} ${style.none}`}><span className={style.icon_again}></span><span>Play again</span></div>
         </div>
-      
       </div>
     </main>
-    <footer className="footer">
+    <footer className={style.footer}>
       <span>App developer: <a href="https://github.com/VedKhorkov">VedKhorkov</a></span>
       <span>Design: <a href="https://www.behance.net/klishinama255b">Mary Kli</a></span>
-      <div className="footer-menu">
-        <div className="icon-home"></div>
-        <div className="icon-categories"></div>
-        <div className="icon-score"></div>
+      <div className={style.footer_menu}>
+        <div className={style.icon_home}></div>
+        <div className={style.icon_categories}></div>
+        <div className={style.icon_score}></div>
       </div>
     </footer>
     </div>
