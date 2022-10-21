228e00fbcd172c2076fe5d8e0c573d29cd062901
vedmond <vyedkh@gmail.com>
2022-10-21 16:36:24 +0300

    feat: add icon in notfoundPage

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -2,12 +2,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import style from '../../scss/NotFound.module.scss'
+import { BsEmojiFrown } from "react-icons/bs";
 
 export const NotFound = () => {
   return (
     <div>
       <div className={style.title}>
-        <div>Page not found !</div>
+        <h2>We are sorry</h2>
+        <h2> Not found <span><BsEmojiFrown fontSize={20}/></span> !</h2>
+        
         <Link to='/'>
          <div className={style.btn}><p>Home</p></div>
         </Link>
